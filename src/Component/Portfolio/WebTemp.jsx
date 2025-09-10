d5ca5d66e6551987180ec7ee2c597af6bf002fd3
unknown <aryan.katiyar@smartschoolonline.in>
2025-09-10 18:55:27 +0530

    scroll to top added

--- src/Component/Portfolio/WebTemp.jsx
+++ src/Component/Portfolio/WebTemp.jsx
@@ -98,13 +98,13 @@ export const AppTemp = () => {
           <div className="relative md:w-5/12">
             <img
               className="rounded-2xl"
-              src="https://images.unsplash.com/photo-1467232004584-a241de8bcf5d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHwxM3x8d2Vic2l0ZXxlbnwwfDB8fHwxNzIwMjI4OTA3fDA&ixlib=rb-4.0.3&q=80&w=1080"
+               src="https://images.unsplash.com/photo-1467232004584-a241de8bcf5d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHwxM3x8d2Vic2l0ZXxlbnwwfDB8fHwxNzIwMjI4OTA3fDA&ixlib=rb-4.0.3&q=80&w=1080"
             />
             <div className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
               <h1 className="text-white mb-3 font-bold text-center uppercase lg:text-xl">
                 Learn Webby
               </h1>
-              <button className="w-full px-6 py-3 text-xs text-white transition duration-300 ease-in-out transform border border-2 rounded-full lg:text-md focus:outline-none hover:scale-110 font-medium">
+              <button className="w-full px-6 py-3 text-xs text-white transition duration-300 ease-in-out transform border  rounded-full lg:text-md focus:outline-none hover:scale-110 font-medium">
                 Go to Docs
               </button>
             </div>
@@ -118,7 +118,7 @@ export const AppTemp = () => {
               <h1 className="text-white mb-3 font-bold text-center uppercase lg:text-xl">
                 Visit Website
               </h1>
-              <button className="w-full px-6 py-3 text-xs text-white transition duration-300 ease-in-out transform bg-indigo-500 border-indigo-400 border border-2 rounded-full lg:text-md focus:outline-none hover:scale-110 font-medium">
+              <button className="w-full px-6 py-3 text-xs text-white transition duration-300 ease-in-out transform bg-indigo-500 border-indigo-400 border  rounded-full lg:text-md focus:outline-none hover:scale-110 font-medium">
                 Go to Website
               </button>
             </div>
